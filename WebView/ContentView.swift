faba913ab0edf8d535c0165f3f9d345cb50863e8
Kevin Marin <kevinmarinsaavedra@gmail.com>
2024-02-16 14:28:18 +0100

    Initial Commit

---
+++ WebView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebView
+//
+//  Created by Kevin Marin on 16/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by Kevin Marin on 16/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
